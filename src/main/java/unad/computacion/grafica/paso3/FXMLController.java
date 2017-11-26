1efedd4797ab7a183dd50e8f9428da6f660283ef
Pablo Salgado <pabloasalgado@gmail.com>
2017-11-26 04:29:40 -0500

    paso 3

--- src/main/java/unad/computacion/grafica/paso3/FXMLController.java
+++ src/main/java/unad/computacion/grafica/paso3/FXMLController.java
@@ -5,21 +5,50 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 public class FXMLController implements Initializable {
+        
+    @FXML
+    private Canvas canvas;
+    
+    private GraphicsContext gc;
     
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void handleButtonLinea(ActionEvent event) {   
+        gc.clearRect(0, 0, 300, 250);
+        gc.strokeLine(40, 10, 10, 40);
     }
+    
+    @FXML
+    private void handleButtonCirculo(ActionEvent event) {        
+        gc.clearRect(0, 0, 300, 250);
+        gc.fillOval(10, 60, 30, 30);
+        gc.strokeOval(60, 60, 30, 30);
+     }
+
+    @FXML
+    private void handleButtonPoligono(ActionEvent event) {        
+        gc.clearRect(0, 0, 300, 250);
+        gc.fillPolygon(
+                new double[]{10, 40, 10, 40},
+                new double[]{210, 210, 240, 240}, 4);
+        gc.strokePolygon(
+                new double[]{60, 90, 60, 90},
+                new double[]{210, 210, 240, 240}, 4);
+        gc.strokePolyline(
+                new double[]{110, 140, 110, 140},
+                new double[]{210, 210, 240, 240}, 4);
+    }
+
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        gc = canvas.getGraphicsContext2D();        
+        gc.setFill(Color.GREEN);
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(5);
     }    
 }
